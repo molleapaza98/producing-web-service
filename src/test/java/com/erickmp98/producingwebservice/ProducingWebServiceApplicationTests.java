79bd3fb471dc5850668940672c91b0667396c4bf
Erick Molleapaza <molleapaza98@gmail.com>
2020-03-20 19:02:36 -0500

    initial commit

---
+++ src/test/java/com/erickmp98/producingwebservice/ProducingWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erickmp98.producingwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducingWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
